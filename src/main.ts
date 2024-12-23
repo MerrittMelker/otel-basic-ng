b370835b448c146d935e9d692d3a741d117e2d76
MerrittMelker <mmelker@tessituranetwork.com>
2024-12-22 18:53:47 -0600

    second commit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,24 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+
+// Initialize OpenTelemetry tracing
+const tracerProvider = new WebTracerProvider();
+
+// Add a Span Processor and Exporter
+tracerProvider.addSpanProcessor(
+  new BatchSpanProcessor(new ConsoleSpanExporter())
+);
+
+// Register the Tracer Provider with global OpenTelemetry APIs
+tracerProvider.register();
+
+// Initialize automatic instrumentation
+getWebAutoInstrumentations();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
